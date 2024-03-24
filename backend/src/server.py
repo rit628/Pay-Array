c8f17593b838905be13a62bcc6125ab885c8700f
Tomas Rivera <tr4462603@gmail.com>
2024-03-24 12:52:55 -0400

    Added urls for retrieving and deleting user data

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import sqlalchemy as sql
-from sqlalchemy.ext.automap import automap_base
 import os
 from .auth import *
 from .orm import db, orm_object_to_dict, User, Item 
+from werkzeug.exceptions import NotFound
 
 def create_app(name=__name__, testing=False):
     AUTH_TYPE = os.environ.get("AUTH_TYPE")
@@ -29,11 +29,33 @@ def create_app(name=__name__, testing=False):
     db.init_app(app)        
 
     def get_auth_token() -> str:
+        """Gets the JWT associated with the user requesting a resource.
+
+        Raises:
+            AuthenticationError: If token is missing or authorization header is invalid. Contains a 401 error code.
+
+        Returns:
+            str: User's JWT for authentication.
+        """
         if request.authorization is None:
             raise AuthenticationError("Missing Token.")
         if request.authorization.type.capitalize() != AUTH_TYPE:
             raise AuthenticationError("Invalid Authorization Header.")
         return request.authorization.token
+
+    def get_request_user() -> User:
+        """Gets the user object associated with the request.
+
+        Raises:
+            NotFound: If a user with the provided id does not exist in the database. Contains a 404 error code.
+
+        Returns:
+            User: User object associated with id provided in authorization header.
+        """
+        token = get_auth_token()
+        id = decode_token(token)["user_id"]
+        user = db.get_or_404(User, id, description="User does not exist.")
+        return user
 
     @app.errorhandler(InvalidFieldError)
     def handle_invalid_field_error(error):
@@ -47,17 +69,17 @@ def create_app(name=__name__, testing=False):
         response.status_code = error.status_code
         return response
 
-    @app.route("/validate-server-runtime", methods=["GET"])
+    @app.route("/validate-server-runtime/", methods=["GET"])
     def validate_server_runtime():
         return jsonify("Hello World!"), 200
 
-    @app.route("/validate-db-connection", methods=["GET"])
+    @app.route("/validate-db-connection/", methods=["GET"])
     def validate_db_connection():
         tables = db.engine.connect().execute(sql.text("SHOW TABLES"))
         result = [tuple(i) for i in tables.fetchall()]
         return jsonify(result), 200
 
-    @app.route(f"{API_ROOT_PATH}/login", methods=["POST"])
+    @app.route(f"{API_ROOT_PATH}/login/", methods=["POST"])
     def login():
         supplied_credentials = request.get_json()
         username = supplied_credentials["username"]
@@ -70,13 +92,13 @@ def create_app(name=__name__, testing=False):
         response = f"{AUTH_TYPE} {token}"
         return jsonify(response), 201
 
-    @app.route(f"{API_ROOT_PATH}/logout", methods=["DELETE"])
+    @app.route(f"{API_ROOT_PATH}/logout/", methods=["DELETE"])
     def logout():
         # TODO: Add a cache database to invalidate tokens post logout
         token = get_auth_token()
         return jsonify("Logout Successful."), 200
 
-    @app.route(f"{API_ROOT_PATH}/users", methods=["POST"])
+    @app.route(f"{API_ROOT_PATH}/users/", methods=["POST"])
     def user_create():
         data = request.get_json()
         if "username" not in data:
@@ -103,8 +125,28 @@ def create_app(name=__name__, testing=False):
         db.session.add(user)
         db.session.commit()
         return jsonify("User Created Successfully."), 201
-
-    @app.route("/debug", methods=["GET"])
+    
+    @app.route(f"{API_ROOT_PATH}/users/me/", methods=["GET", "DELETE"])
+    def user_access():
+        user = get_request_user()
+        if request.method == "GET":
+            user_dict = orm_object_to_dict(user)
+            return jsonify(user_dict), 200
+        elif request.method == "DELETE":
+            db.session.delete(user)
+            db.session.commit()
+            return jsonify("User Deleted Successfully"), 200
+    
+    @app.route(f"{API_ROOT_PATH}/users/me/<resource>/", methods=["GET"])
+    def user_get(resource:str):
+        user = get_request_user()
+        user_dict = orm_object_to_dict(user)
+        if resource not in user_dict:
+            raise NotFound("Requested resource does not exist.")
+        fetched_resource = orm_object_to_dict(user)[resource]
+        return jsonify(fetched_resource), 200
+    
+    @app.route("/debug/", methods=["GET"])
     def debug():
         statement = sql.select(User)
         users = db.session.scalars(statement).all()
