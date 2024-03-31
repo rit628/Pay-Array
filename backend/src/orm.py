a3e14aec26fdc99d53890c9c6d95eff34b0b49ba
Tomas Rivera <tr4462603@gmail.com>
2024-03-30 20:10:36 -0400

    Updated orm to fix user item mapping

--- backend/src/orm.py
+++ backend/src/orm.py
@@ -42,6 +42,11 @@ class Base(db.Model):
         rep_str += "\b\b)"
         return rep_str
 
+user_preference = db.Table('user_preference',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('item_id', db.Integer, db.ForeignKey('item.id'), primary_key=True)
+)
+
 class User(Base):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True, nullable=False)
@@ -54,15 +59,12 @@ class User(Base):
     last_name = db.Column(db.String(50))
     phone = db.Column(db.String(10))
     budget = db.Column(db.DECIMAL)
+    items = db.relationship('Item', secondary=user_preference, back_populates="users")
 
 class Item(Base):
     __tablename__ = "item"
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    name = db.Column(db.String(100))
+    name = db.Column(db.String(100), nullable=False, unique=True)
     price = db.Column(db.DECIMAL)
     purchase_link = db.Column(db.String(2048))
-    
-user_preference = db.Table('user_preference',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-    db.Column('item_id', db.Integer, db.ForeignKey('item.id'), primary_key=True)
-)
+    users = db.relationship('User', secondary=user_preference, back_populates="items")
