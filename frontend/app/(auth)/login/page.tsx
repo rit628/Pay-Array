e579d1784e75813c2312168de45233bf4d125b4b
saras1212 <sarasmith1@ufl.edu>
2024-04-17 17:49:40 -0400

    created the userPage and payRequest page, connected these to existing pages

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -75,7 +75,9 @@ const LoginPage: React.FC = () => {
                   required
                 />
               </div>
+              <Link href="/userPage">
               <button type="submit" className="submit-button">Login</button>
+              </Link>
             </form>
           </div>
         </div>
