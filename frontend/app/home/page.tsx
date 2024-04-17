ec19f4a144374f05f922bd86ddee1770675cd419
Catrina lim <92826935+catrinalim@users.noreply.github.com>
2024-04-17 18:07:00 -0400

    Navigation and Pages (#5)
    * navBar test
    
    * fixed navbar
    
    * fixed style.css
    
    * fixed styling
    
    * fixed tabs
    
    * new pages

---
+++ frontend/app/home/page.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Navigation from "../components/navigation/navigationBar";
+
+const Home = () => {
+  return (
+    <>
+      <title>Home | PayArray</title>
+      <div className="container">
+        <h1>Home</h1>
+        <p>Split costs with your rommates effortlessly</p>
+      </div>
+    </>
+  );
+};
+
+export default Home;
