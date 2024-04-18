866609915d1c9410a16da9f8dad5b1e16d93ec01
saras1212 <sarasmith1@ufl.edu>
2024-04-18 11:22:33 -0400

    redid the pay page to prepare for dropdowns

--- frontend/app/pay/page.tsx
+++ frontend/app/pay/page.tsx
@@ -4,7 +4,6 @@ import React, { useState } from 'react';
 
 const Pay: React.FC = () => {
   const [formData, setFormData] = useState({
-    amount: '',
     payTo: '',
     msg: '',
   });
@@ -23,53 +22,6 @@ const Pay: React.FC = () => {
   return (
     <div className='container'>
       <h1 className='payReqTitle'>Let's Pay!</h1>
-      <form onSubmit={handleSubmit} className="form">
-      <div className='form-group'>
-      <label htmlFor="amount" className="amount">
-            $:
-          </label>
-          <input
-            type="text"
-            id="amount"
-            name="amount"
-            value={formData.amount}
-            onChange={handleChange}
-            className="input"
-            placeholder=" "
-            required
-          />
-      </div>
-        <div className="form-group">
-          <label htmlFor="payTo" className="payTo">
-            To:
-          </label>
-          <input
-            type="text"
-            id="payTo"
-            name="payTo"
-            value={formData.payTo}
-            onChange={handleChange}
-            className="input"
-            placeholder="Username"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="msg" className="msg">
-            Message:
-          </label>
-          <input
-            type="text"
-            id="msg"
-            name="msg"
-            value={formData.msg}
-            onChange={handleChange}
-            className="input"
-            placeholder="What's this for?"
-            required
-          />
-        </div>
-      </form>
       <div className='payReqButtons'>
         <button className='payReqButton w-full'>Pay</button>
       </div>
--- frontend/app/request/page.tsx
+++ frontend/app/request/page.tsx
@@ -1,18 +1,80 @@
-import Head from "next/head";
+'use client'
+import React, { useState } from 'react';
 
-const Request = () => {
+
+const Pay: React.FC = () => {
+  const [formData, setFormData] = useState({
+    amount: '',
+    payTo: '',
+    msg: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+    // logic
+  };
+
   return (
-        /*need some input tag for money, need to input area, reason input area*/
-        <div className = 'container'>
-            <h1 className= 'payReqTitle'>Let's Request!</h1>
-        <div className='moneyAmount'>
-            <p>$</p>
+    <div className='container'>
+      <h1 className='payReqTitle'>Let's Pay!</h1>
+      <form onSubmit={handleSubmit} className="form">
+      <div className='form-group'>
+      <label htmlFor="amount" className="amount">
+            $:
+          </label>
+          <input
+            type="text"
+            id="amount"
+            name="amount"
+            value={formData.amount}
+            onChange={handleChange}
+            className="input"
+            placeholder=" "
+            required
+          />
+      </div>
+        <div className="form-group">
+          <label htmlFor="payTo" className="payTo">
+            To:
+          </label>
+          <input
+            type="text"
+            id="payTo"
+            name="payTo"
+            value={formData.payTo}
+            onChange={handleChange}
+            className="input"
+            placeholder="Username"
+            required
+          />
         </div>
-        <div className= 'payReqButtons'>
-        <button className= 'payReqButton w-full'>Request</button>
+        <div className="form-group">
+          <label htmlFor="msg" className="msg">
+            Message:
+          </label>
+          <input
+            type="text"
+            id="msg"
+            name="msg"
+            value={formData.msg}
+            onChange={handleChange}
+            className="input"
+            placeholder="What's this for?"
+            required
+          />
         </div>
-        </div>
+      </form>
+      <div className='payReqButtons'>
+        <button className='payReqButton w-full'>Pay</button>
+      </div>
+    </div>
   );
 };
 
-export default Request;
+export default Pay;
