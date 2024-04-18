e60e89ffaad4976de9274efa107aee1f50e9293b
saras1212 <sarasmith1@ufl.edu>
2024-04-18 11:22:33 -0400

    redid the pay page to prepare for dropdowns

--- frontend/app/pay/page.tsx
+++ frontend/app/pay/page.tsx
@@ -7,7 +7,6 @@ import React, { useState } from 'react';
 
 const Pay: React.FC = () => {
   const [formData, setFormData] = useState({
-    amount: '',
     payTo: '',
     msg: '',
   });
@@ -58,53 +57,6 @@ const Pay: React.FC = () => {
   return (
     <div className='container'>
       <h1 className='payReqTitle'>Let's Pay!</h1>
-      <form onSubmit={handleSubmit} className="form">
-      <div className='form-group'>
-      <label htmlFor="amount" className="amount">
-            $:
-          </label>
-          <input
-            type="text"
-            id="amount"
-            name="amount"
-            value={formData.amount}
-            onChange={handleChange}
-            className="input"
-            placeholder=" "
-            required
-          />
-      </div>
-        <div className="form-group">
-          <label htmlFor="payTo" className="payTo">
-            To:
-          </label>
-          <input
-            type="text"
-            id="payTo"
-            name="payTo"
-            value={formData.payTo}
-            onChange={handleChange}
-            className="input"
-            placeholder="Username"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="msg" className="msg">
-            Message:
-          </label>
-          <input
-            type="text"
-            id="msg"
-            name="msg"
-            value={formData.msg}
-            onChange={handleChange}
-            className="input"
-            placeholder="What's this for?"
-            required
-          />
-        </div>
-      </form>
       <div className='payReqButtons'>
       <DynamicSingleSelectDropdown options={householdUsers} label="Select User" onSelect={handleSelect} />
         <button className='payReqButton w-full'>Pay</button>
