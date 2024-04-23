e81b21a417e12f47c7590361b8c0b4195eddc1ca
Tomas Rivera <tr4462603@gmail.com>
2024-04-22 22:00:35 -0400

    Added API connections for household operations

--- frontend/app/household/page.tsx
+++ frontend/app/household/page.tsx
@@ -1,10 +1,28 @@
 'use client'
-import { useState } from 'react'
+import { useState, useEffect } from "react"; 
 
 const Household = () => {
+
+  const [householdId, setHouseholdId] = useState(null);
+  const header : any = localStorage.getItem('auth-header');
+  useEffect(() => {
+    const getHouseholdId = async () => {
+      const response = await fetch(`${process.env.API_URL}/users/me/household_id/`, {
+        "method": "GET",
+        "mode": "cors",
+        "headers": {
+          "Content-Type": "application/json",
+          "Authorization": header
+        },
+      });
+      const householdId = await response.json();
+      setHouseholdId(householdId);
+    };
+    getHouseholdId();
+  }, []);
     return (
        <div className='container'>
-        <p className='householdTitle'>Your Household:</p>
+        <p className='householdTitle'>Your Household: {householdId}</p>
        </div>
         
 
